8942646acee58cc63e350555e740a129ebe7d322
Anton Yeshchenko <anton.yeshchenko@gmail.com>
2019-12-18 16:20:47 +0100

    change the folder system for files

--- src/auxiliary/constant_definitions.py
+++ src/auxiliary/constant_definitions.py
@@ -1,24 +1,31 @@
 from pathlib import Path
 
+
 ROOT_DIR =  Path(__file__).parent.parent.parent # This is your Project Root
 
-FOLDER_DATA_INITIAL_LOGS = Path(ROOT_DIR) / 'data' / 'data_initial'
-FOLDER_DATA_TIMESTAMP_SORTED_LOGS = Path(ROOT_DIR) / 'data' / "data_intermediate" / "timestamp_sorted_logs"
 
-FOLDER_DATA_MINERFUL_DECLARE_CONSTRAINTS = Path(ROOT_DIR) / 'data' / "data_intermediate" / "declare_from_minerful"
-FOLDER_TIMESTAMP_TICKS_FOR_GRAPHS = Path(ROOT_DIR) / 'data' / "data_intermediate" / "timestamp_ticks_for_graphs"
+FOLDER_DATA = Path(ROOT_DIR) / 'data'
 
-FOLDER_GRAPHS_DRIFT_MAPS = Path(ROOT_DIR) / 'data' / "data_results_final" / "graphs" / "drift_maps"
+FOLDER_DATA_INITIAL_LOGS = FOLDER_DATA / 'data_initial'
+FOLDER_DATA_INTERMEDIATE = FOLDER_DATA / "data_intermediate"
+FOLDER_DATA_RESULTS_FINAL = FOLDER_DATA / "data_results_final"
 
-FOLDER_GRAPHS_DRIFT_PLOTS = Path(ROOT_DIR) / 'data' / "data_results_final" / "graphs" / "drift_plots"
+FOLDER_DATA_TIMESTAMP_SORTED_LOGS = Path("timestamp_sorted_logs")
+
+FOLDER_DATA_MINERFUL_DECLARE_CONSTRAINTS = Path("declare_from_minerful")
+FOLDER_TIMESTAMP_TICKS_FOR_GRAPHS = Path("timestamp_ticks_for_graphs")
+
+FOLDER_GRAPHS_DRIFT_MAPS = Path("graphs") / "drift_maps"
+
+FOLDER_GRAPHS_DRIFT_PLOTS = Path("graphs") / "drift_plots"
 FOLDER_GRAPHS_DRIFT_PLOTS_PLOTS = FOLDER_GRAPHS_DRIFT_PLOTS / 'plots'
 FOLDER_GRAPHS_DRIFT_AVERAGED_TIMESERIES = FOLDER_GRAPHS_DRIFT_PLOTS / 'timeseries'
 FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS = FOLDER_GRAPHS_DRIFT_PLOTS / "constraints_in_clusters"
 FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS_PRUNED = FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS / "constraints_pruned"
 
-FOLDER_GRAPHS_DFG = Path(ROOT_DIR) / 'data' / "data_results_final" / "graphs" / "dfg_visualizations"
+FOLDER_GRAPHS_DFG = Path("graphs") / "dfg_visualizations"
 
-FOLDER_GRAPHS_STATISTICS = Path(ROOT_DIR) / 'data' / "data_results_final" / "statistics"
+FOLDER_GRAPHS_STATISTICS = Path("statistics")
 
 XES_FILE_EXTENSION = '.xes'
 CSV_FILE_EXTENSION = '.csv'
--- src/auxiliary/data_structures.py
+++ src/auxiliary/data_structures.py
@@ -5,8 +5,7 @@ from src.auxiliary.constant_definitions import FOLDER_DATA_TIMESTAMP_SORTED_LOGS
     FOLDER_TIMESTAMP_TICKS_FOR_GRAPHS, PNG_FILE_EXTENSION, FOLDER_GRAPHS_DRIFT_MAPS, FOLDER_GRAPHS_STATISTICS, \
     FOLDER_GRAPHS_DRIFT_AVERAGED_TIMESERIES, JSON_FILE_EXTENSION, \
     FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS, FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS_PRUNED, \
-    FOLDER_GRAPHS_DRIFT_PLOTS_PLOTS, FOLDER_GRAPHS_DFG
-
+    FOLDER_GRAPHS_DRIFT_PLOTS_PLOTS, FOLDER_GRAPHS_DFG, FOLDER_DATA_INTERMEDIATE, FOLDER_DATA_RESULTS_FINAL
 
 class FilesManagement:
     def __init__(self, log_name, algoPrmt):
@@ -56,44 +55,54 @@ class FilesManagement:
         return FOLDER_DATA_INITIAL_LOGS / compose_name(self.log_name,XES_FILE_EXTENSION)
 
     def get_path_input_sorted_xes(self):
-        ensure_path_exists(FOLDER_DATA_TIMESTAMP_SORTED_LOGS)
-        return FOLDER_DATA_TIMESTAMP_SORTED_LOGS / compose_name(self.log_name, XES_FILE_EXTENSION)
+        fdtms = FOLDER_DATA_INTERMEDIATE / self.log_name / FOLDER_DATA_TIMESTAMP_SORTED_LOGS
+        ensure_path_exists(fdtms)
+        return fdtms / compose_name(self.log_name, XES_FILE_EXTENSION)
 
     def get_path_minerful_constraints(self):
-        ensure_path_exists(FOLDER_DATA_MINERFUL_DECLARE_CONSTRAINTS)
-        return FOLDER_DATA_MINERFUL_DECLARE_CONSTRAINTS / self.namefile_minerful_constr
+        fdmdc = FOLDER_DATA_INTERMEDIATE / self.log_name / FOLDER_DATA_MINERFUL_DECLARE_CONSTRAINTS
+        ensure_path_exists(fdmdc)
+        return fdmdc / self.namefile_minerful_constr
 
     def get_path_timestamp_ticks(self):
-        ensure_path_exists(FOLDER_TIMESTAMP_TICKS_FOR_GRAPHS)
-        return FOLDER_TIMESTAMP_TICKS_FOR_GRAPHS / self.namefile_timestamp_ticks
+        fttfg = FOLDER_DATA_INTERMEDIATE / self.log_name / FOLDER_TIMESTAMP_TICKS_FOR_GRAPHS
+        ensure_path_exists(fttfg)
+        return fttfg / self.namefile_timestamp_ticks
 
     def get_path_drift_map(self):
-        ensure_path_exists(FOLDER_GRAPHS_DRIFT_MAPS)
-        return FOLDER_GRAPHS_DRIFT_MAPS / self.name_file_drift_map
+        fgdm = FOLDER_DATA_RESULTS_FINAL / self.log_name / FOLDER_GRAPHS_DRIFT_MAPS
+        ensure_path_exists(fgdm)
+        return fgdm / self.name_file_drift_map
 
     def get_path_erratic_measures(self):
-        ensure_path_exists(FOLDER_GRAPHS_STATISTICS)
-        return FOLDER_GRAPHS_STATISTICS / compose_name(self.name_file_erratic, CSV_FILE_EXTENSION)
+        fgs = FOLDER_DATA_RESULTS_FINAL / self.log_name / FOLDER_GRAPHS_STATISTICS
+        ensure_path_exists(fgs)
+        return fgs / compose_name(self.name_file_erratic, CSV_FILE_EXTENSION)
 
     def get_path_drift_plot_averaged_timeseries(self, i):
-        ensure_path_exists(FOLDER_GRAPHS_DRIFT_AVERAGED_TIMESERIES)
-        return FOLDER_GRAPHS_DRIFT_AVERAGED_TIMESERIES / (str(i) + self.name_file_erratic + CSV_FILE_EXTENSION)
+        fgdat = FOLDER_DATA_RESULTS_FINAL / self.log_name / FOLDER_GRAPHS_DRIFT_AVERAGED_TIMESERIES
+        ensure_path_exists(fgdat)
+        return fgdat / (str(i) + self.name_file_erratic + CSV_FILE_EXTENSION)
 
     def get_path_drift_plot(self, i):
-        ensure_path_exists(FOLDER_GRAPHS_DRIFT_PLOTS_PLOTS)
-        return FOLDER_GRAPHS_DRIFT_PLOTS_PLOTS / (str(i) + self.name_file_erratic + PNG_FILE_EXTENSION)
+        fgdpp = FOLDER_DATA_RESULTS_FINAL / self.log_name / FOLDER_GRAPHS_DRIFT_PLOTS_PLOTS
+        ensure_path_exists(fgdpp)
+        return fgdpp / (str(i) + self.name_file_erratic + PNG_FILE_EXTENSION)
 
     def get_path_drift_plot_all_timeseries(self, i):
-        ensure_path_exists(FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS)
-        return FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS / (str(i) + self.name_file_erratic + JSON_FILE_EXTENSION)
+        fgdacic = FOLDER_DATA_RESULTS_FINAL / self.log_name / FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS
+        ensure_path_exists(fgdacic)
+        return fgdacic / (str(i) + self.name_file_erratic + JSON_FILE_EXTENSION)
 
     def get_path_drift_plot_all_timeseries_pruned(self, i):
-        ensure_path_exists(FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS_PRUNED)
-        return FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS_PRUNED / (str(i) + self.name_file_erratic + CSV_FILE_EXTENSION)
+        fgdacicp = FOLDER_DATA_RESULTS_FINAL / self.log_name / FOLDER_GRAPHS_DRIFT_ALL_CONSTRAINTS_IN_CLUSTERS_PRUNED
+        ensure_path_exists(fgdacicp)
+        return fgdacicp / (str(i) + self.name_file_erratic + CSV_FILE_EXTENSION)
 
     def get_path_dfg_visualization_for_constraints(self, i):
-        ensure_path_exists(FOLDER_GRAPHS_DFG)
-        return FOLDER_GRAPHS_DFG / (str(i) + self.name_file_erratic + PNG_FILE_EXTENSION)
+        fgd = FOLDER_DATA_RESULTS_FINAL / self.log_name / FOLDER_GRAPHS_DFG
+        ensure_path_exists(fgd)
+        return fgd / (str(i) + self.name_file_erratic + PNG_FILE_EXTENSION)
 
 
 class AlgorithmParameters:
