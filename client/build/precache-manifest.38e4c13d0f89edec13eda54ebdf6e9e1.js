636e055349157cfa0aaf18cb0890fc7cc6deff33
MarinaZadoyanchuk <marina.zadoyanchuk@gmail.com>
2020-07-29 14:35:40 +0300

    change path to builded fules

---
+++ client/build/precache-manifest.38e4c13d0f89edec13eda54ebdf6e9e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b582410039b13d17da790119ffdeede6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c017e452ebc2d8e1d63",
+    "url": "./static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "8c83bf04256d6bedbb1e",
+    "url": "./static/css/main.d185d039.chunk.css"
+  },
+  {
+    "revision": "5c017e452ebc2d8e1d63",
+    "url": "./static/js/2.6c9b29ca.chunk.js"
+  },
+  {
+    "revision": "67b2218b34fc0c291394ab1ce7aeb55f",
+    "url": "./static/js/2.6c9b29ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c83bf04256d6bedbb1e",
+    "url": "./static/js/main.24cbc4b4.chunk.js"
+  },
+  {
+    "revision": "f35dbb30bb93cebd5c82",
+    "url": "./static/js/runtime-main.f63e6028.js"
+  }
+]);
