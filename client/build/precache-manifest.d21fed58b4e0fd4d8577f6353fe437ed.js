371bab3d42aa30f9452cc271c3d683d037cb3c17
MarinaZadoyanchuk <marina.zadoyanchuk@gmail.com>
2020-07-29 13:56:22 +0300

    build

---
+++ client/build/precache-manifest.d21fed58b4e0fd4d8577f6353fe437ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a94e53e3a97acbd6f1272e84f67104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2c367b7e494f75dfdb",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "45bb0c9a4632ade2c90e",
+    "url": "/static/css/main.d185d039.chunk.css"
+  },
+  {
+    "revision": "4a2c367b7e494f75dfdb",
+    "url": "/static/js/2.f5103073.chunk.js"
+  },
+  {
+    "revision": "67b2218b34fc0c291394ab1ce7aeb55f",
+    "url": "/static/js/2.f5103073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45bb0c9a4632ade2c90e",
+    "url": "/static/js/main.cbc0cf05.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
