00e76247fe57f7e8acab1b02ec59cb461120f538
Anton Yeshchenko <anton.yeshchenko@gmail.com>
2020-07-29 14:36:53 +0200

    add some starting text

--- client/src/components/UploadComponent/index.tsx
+++ client/src/components/UploadComponent/index.tsx
@@ -43,7 +43,12 @@ export const UploadComponent = () => {
     <div className="Upload-area">
       <Empty
         description={
-          <Typography.Title level={2}>TEXT TO START!</Typography.Title>
+          <Typography.Title level={2}>Process Drift Detection with Visual Analytics. 
+                                        Our technique supports the discovery of process drifts in the processes from 
+                                        event logs in several different visual representations!
+                                      
+                                        
+                                        </Typography.Title>
         }
         image={Empty.PRESENTED_IMAGE_SIMPLE}
       >
