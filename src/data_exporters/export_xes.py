1f736f1550f41b72a567b88ae6da1c126c998d0e
Anton Yeshchenko <anton.yeshchenko@gmail.com>
2021-01-05 16:09:59 +0100

    Update export_xes.py

--- src/data_exporters/export_xes.py
+++ src/data_exporters/export_xes.py
@@ -1,4 +1,4 @@
-from pm4py.objects.log.exporter.xes import factory as xes_exporter
+from pm4py.objects.log.exporter.xes import exporter as xes_exporter
 
 def export_xes_log(log, fileMngm):
     xes_exporter.export_log(log, str(fileMngm.get_path_input_sorted_xes()))
